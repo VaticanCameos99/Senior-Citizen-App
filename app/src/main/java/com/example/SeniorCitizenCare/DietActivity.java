d6df768c69a66021f2b31da4311d7ef027931f80
Swapnil <swapnil.sam.13@gmail.com>
2019-10-13 20:00:21 +0530

    Added Diet Section

--- app/src/main/java/com/example/SeniorCitizenCare/DietActivity.java
+++ app/src/main/java/com/example/SeniorCitizenCare/DietActivity.java
@@ -1,14 +1,93 @@
 package com.example.SeniorCitizenCare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class DietActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<String> mList;
+    MyAdapterDietClass adapterDietClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet);
+
+        populateList();
+        buildRecyclerView();
+        onClick();
+
+    }
+
+    public void populateList(){
+        mList = new ArrayList<>();
+
+        mList.add("Diet for Diabetes");
+        mList.add("Diet for Heart Issues");
+        mList.add("Diet for Weight Gain");
+        mList.add("Diet for Weight Loss");
+        mList.add("Diet for Diarrhea");
+        mList.add("Diet for Vomiting");
+        mList.add("Diet for Acidity");
+
+    }
+
+    public void buildRecyclerView(){
+        recyclerView = (RecyclerView) findViewById(R.id.dietRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapterDietClass = new MyAdapterDietClass(mList);
+        recyclerView.setAdapter(adapterDietClass);
+
+        adapterDietClass.setOnItemClickListener(new MyAdapterDietClass.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+
+            }
+        });
+    }
+
+    public void onClick(){
+        adapterDietClass.setOnItemClickListener(new MyAdapterDietClass.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                String dietName = mList.get(position);
+
+//                Intent intent = new Intent(DietActivity.this, DietDetailsActivity.class);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+
+                if(dietName=="Diet for Diabetes")
+                    intent.setData(Uri.parse("https://www.healthline.com/nutrition/16-best-foods-for-diabetics#section1"));
+
+                if(dietName=="Diet for Heart Issues")
+                    intent.setData(Uri.parse("https://www.healthline.com/nutrition/heart-healthy-foods"));
+
+                if(dietName=="Diet for Weight Gain")
+                    intent.setData(Uri.parse("https://www.healthline.com/nutrition/18-foods-to-gain-weight"));
+
+                if(dietName=="Diet for Weight Loss")
+                    intent.setData(Uri.parse("https://www.healthline.com/nutrition/20-most-weight-loss-friendly-foods"));
+
+                if(dietName=="Diet for Diarrhea")
+                    intent.setData(Uri.parse("https://www.healthline.com/health/what-to-eat-when-you-have-diarrhea"));
+
+                if(dietName=="Diet for Vomiting")
+                    intent.setData(Uri.parse("https://www.livestrong.com/article/330220-the-best-foods-to-eat-after-throwing-up/"));
+
+                if(dietName=="Diet for Acidity")
+                    intent.setData(Uri.parse("https://www.healthline.com/health/gerd/diet-nutrition"));
+
+//                intent.putExtra("dietName", dietName);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/SeniorCitizenCare/MyAdapterDietClass.java
+++ app/src/main/java/com/example/SeniorCitizenCare/MyAdapterDietClass.java
@@ -1,21 +1,18 @@
 package com.example.SeniorCitizenCare;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class MyAdapterContactSmallClass extends RecyclerView.Adapter<MyAdapterContactSmallClass.MyViewHolder> {
+public class MyAdapterDietClass extends RecyclerView.Adapter<MyAdapterDietClass.MyViewHolder> {
 
-    ArrayList<ContactClassSmall> mList;
+    ArrayList<String> mList;//TODO
     OnItemClickListener mListener;
 
     public interface OnItemClickListener{
@@ -26,29 +23,23 @@ public class MyAdapterContactSmallClass extends RecyclerView.Adapter<MyAdapterCo
         mListener=listener;
     }
 
-    public MyAdapterContactSmallClass(ArrayList<ContactClassSmall> mList) {
+    public MyAdapterDietClass(ArrayList<String> mList) {
+        //TODO
         this.mList = mList;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_contacts, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_diet, parent, false);
         return new MyViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        ContactClassSmall contact = mList.get(position);
+        String diet = mList.get(position);
 
-        //TODO
-//        if(holder==null){
-//            Log.i("Info", "Ugh");
-//        }
-        holder.mContactImageView.setImageResource(contact.getmImageResource());
-        holder.mContactName.setText(contact.getmName());
-        holder.mContactNumber.setText(contact.getmNumber());
-
+        holder.dietName.setText(diet);
     }
 
     @Override
@@ -56,18 +47,14 @@ public class MyAdapterContactSmallClass extends RecyclerView.Adapter<MyAdapterCo
         return mList.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder{
 
-        ImageView mContactImageView;
-        TextView mContactName;
-        TextView mContactNumber;
+        TextView dietName;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            mContactImageView = itemView.findViewById(R.id.imageContact);
-            mContactName = itemView.findViewById(R.id.textContactName);
-            mContactNumber = itemView.findViewById(R.id.textContactNumber);
+            dietName = itemView.findViewById(R.id.diet_name);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -81,10 +68,5 @@ public class MyAdapterContactSmallClass extends RecyclerView.Adapter<MyAdapterCo
                 }
             });
         }
-    }
-
-    public void filterList(ArrayList<ContactClassSmall> filteredList){
-        mList = filteredList;
-        notifyDataSetChanged();
     }
 }
