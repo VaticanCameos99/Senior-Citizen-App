7e2c770f9faf6010e593e0fc1471429890b83e1a
apurvawani <apurva.wani06@gmail.com>
2019-10-13 17:52:54 +0530

    Alarm Notification Added

---
+++ app/src/main/java/com/example/SeniorCitizenCare/AlarmReceiver.java
@@ -0,0 +1,19 @@
+package com.example.SeniorCitizenCare;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.widget.Toast;
+
+public class AlarmReceiver extends  BroadcastReceiver {
+    @Override
+    public void onReceive(Context context , Intent intent) {
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+        Ringtone r = RingtoneManager.getRingtone(context ,  notification);
+        r.play();
+        Toast.makeText(context , "It's  time for medicine!", Toast.LENGTH_LONG).show();
+    }
+}
